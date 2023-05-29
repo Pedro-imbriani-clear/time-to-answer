bb11f28c2dc7271ea8b5cc3b82ffabc86c9f69a7
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-05-29 10:38:02 -0300

    password recovery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Mailtrap
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '2b9e734586ac7c',
+    :password => 'dae03bd42885b8',
+    :address => 'sandbox.smtp.mailtrap.io',
+    :domain => 'sandbox.smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
