528aed2ed2c3671a5e93050370ae04000b718d0d
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-05-29 10:05:18 -0300

    Admin Statistic

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,22 +3,29 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  after_create :set_statistic
   paginates_per 5
 
   scope :_search_subject_, ->(page,subject_id){
     includes(:answers, :subject)
-            .where(subject_id: subject_id)
-            .page(page)
+      .where(subject_id: subject_id)
+      .page(page)
   }
 
   scope :_search_, ->(page,term){
     includes(:answers,:subject)
-            .where("lower(descriptions) LIKE ?", "%#{term.downcase}%"
-            .page(page))
+      .where("lower(descriptions) LIKE ?", "%#{term.downcase}%"
+      .page(page))
   }
 
- scope :last_questions, ->(page){
+  scope :last_questions, ->(page){
     Question.include(:answers, :subject).order("created_at desc")
-                              .page(params[:page])
- }
+                                        .page(params[:page])
+  }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[total_questions])
+  end
 end
